d400fdec1a3cb3509ac2b4992aae7d379d904dcb
Roy Wolfe <mail@royderwolf.de>
2024-05-25 20:34:06 +0100

    Update fork with latest changes.
    Reintroduces mouse keys PR
    
    # Conflicts:
    #	.github/workflows/build-example-corneish_zen-custom.yml
    #	.github/workflows/build-example-mousekeyspr.yml
    #	.github/workflows/test-all-boards.yml
    #	.github/workflows/test-all-configs.yml
    #	.github/workflows/test-all-xiao-shields.yml
    #	.github/workflows/test-inputs.yml

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,5 @@
-// Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
+#define MIRYOKU_LAYER_EXTRA \
+&kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             \
+&kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             \
+&kp LCTRL,         &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           \
+U_NP,              U_NP,              &kp LALT,          &kp SPC,           &mo U_TAP,         &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
